5173eee81b395f9b2e016e16f7d5adeed05904ae
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2025-08-04 08:44:17 -0300

    fix: zod error message parser

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,38 +1,28 @@
 import type { NextFunction, Request, Response } from "express";
-import type { ZodType } from "zod";
-import { capitalize } from "../helpers/convert.helper";
+import { ZodError, type ZodIssue, type ZodType } from "zod";
 import { CustomError } from "../models/error.type";
 
-// biome-ignore lint: any
-const extractErrorMessage = (err: any) => {
-  const { path, received, message, expected } = err;
+const extractErrorMessage = (err: ZodIssue): string => {
+  const { path, message } = err;
+  const fieldPath = path.join(".");
 
-  const erroMessage = `Erro:'${message}'.${expected ? ` Esperado:'${capitalize(expected)}'` : ""}`;
-
-  if (received === undefined) {
-    return `O campo '${path}' é obrigatório. ${erroMessage}`;
-  }
-
-  return `O campo '${path}' recebeu '${received}'. ${erroMessage}`;
+  if (err.code === "custom") return message;
+  if (path.length > 0) return `${fieldPath}: ${message}`;
+  return message;
 };
 
 const validate = (schema: ZodType, type: "body" | "params" | "query") => {
   return (req: Request, _res: Response, next: NextFunction) => {
     try {
       schema.parse(req[type]);
-
       next();
-      // biome-ignore lint: any
-    } catch (e: any) {
-      if (e.errors) {
-        const errArray = e.errors;
-        for (const err of errArray) {
-          const errMessage = extractErrorMessage(err);
-
-          next(new CustomError(errMessage, 400));
-        }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const firstError = error.issues[0];
+        const errMessage = extractErrorMessage(firstError);
+        next(new CustomError(errMessage, 400));
       } else {
-        next(new CustomError(e.message, 400));
+        next(new CustomError("Erro de validação", 400));
       }
     }
   };
